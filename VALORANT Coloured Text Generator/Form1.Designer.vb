b33196328cebba8c7513d54b2b23190ed06a28fe
Burak <80233029+SWLS1Developer@users.noreply.github.com>
2021-06-11 12:38:32 +0300

    Add files via upload

---
+++ VALORANT Coloured Text Generator/Form1.Designer.vb
@@ -0,0 +1,298 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.clsBrackets = New System.Windows.Forms.RadioButton()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.pink = New System.Windows.Forms.RadioButton()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.green = New System.Windows.Forms.RadioButton()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.yellow = New System.Windows.Forms.RadioButton()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.blue = New System.Windows.Forms.RadioButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.red = New System.Windows.Forms.RadioButton()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.input = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox6 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox6.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 237)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(259, 36)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(63, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(132, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Discord: Developer#2798 "
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ComboBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(146, 51)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Language"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Türkçe ", "English"})
+        Me.ComboBox1.Location = New System.Drawing.Point(9, 19)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.clsBrackets)
+        Me.GroupBox3.Controls.Add(Me.Panel5)
+        Me.GroupBox3.Controls.Add(Me.pink)
+        Me.GroupBox3.Controls.Add(Me.Panel4)
+        Me.GroupBox3.Controls.Add(Me.green)
+        Me.GroupBox3.Controls.Add(Me.Panel3)
+        Me.GroupBox3.Controls.Add(Me.yellow)
+        Me.GroupBox3.Controls.Add(Me.Panel2)
+        Me.GroupBox3.Controls.Add(Me.blue)
+        Me.GroupBox3.Controls.Add(Me.Panel1)
+        Me.GroupBox3.Controls.Add(Me.red)
+        Me.GroupBox3.Location = New System.Drawing.Point(164, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(363, 51)
+        Me.GroupBox3.TabIndex = 3
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Colors"
+        '
+        'clsBrackets
+        '
+        Me.clsBrackets.AutoSize = True
+        Me.clsBrackets.Location = New System.Drawing.Point(272, 20)
+        Me.clsBrackets.Name = "clsBrackets"
+        Me.clsBrackets.Size = New System.Drawing.Size(51, 17)
+        Me.clsBrackets.TabIndex = 13
+        Me.clsBrackets.TabStop = True
+        Me.clsBrackets.Text = "Close"
+        Me.clsBrackets.UseVisualStyleBackColor = True
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Panel5.Location = New System.Drawing.Point(237, 20)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(16, 16)
+        Me.Panel5.TabIndex = 11
+        '
+        'pink
+        '
+        Me.pink.AutoSize = True
+        Me.pink.Location = New System.Drawing.Point(220, 22)
+        Me.pink.Name = "pink"
+        Me.pink.Size = New System.Drawing.Size(14, 13)
+        Me.pink.TabIndex = 12
+        Me.pink.TabStop = True
+        Me.pink.UseVisualStyleBackColor = True
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Panel4.Location = New System.Drawing.Point(184, 20)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(16, 16)
+        Me.Panel4.TabIndex = 9
+        '
+        'green
+        '
+        Me.green.AutoSize = True
+        Me.green.Location = New System.Drawing.Point(167, 22)
+        Me.green.Name = "green"
+        Me.green.Size = New System.Drawing.Size(14, 13)
+        Me.green.TabIndex = 10
+        Me.green.TabStop = True
+        Me.green.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Panel3.Location = New System.Drawing.Point(129, 20)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(16, 16)
+        Me.Panel3.TabIndex = 7
+        '
+        'yellow
+        '
+        Me.yellow.AutoSize = True
+        Me.yellow.Location = New System.Drawing.Point(112, 22)
+        Me.yellow.Name = "yellow"
+        Me.yellow.Size = New System.Drawing.Size(14, 13)
+        Me.yellow.TabIndex = 8
+        Me.yellow.TabStop = True
+        Me.yellow.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Panel2.Location = New System.Drawing.Point(77, 20)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(16, 16)
+        Me.Panel2.TabIndex = 2
+        '
+        'blue
+        '
+        Me.blue.AutoSize = True
+        Me.blue.Location = New System.Drawing.Point(60, 22)
+        Me.blue.Name = "blue"
+        Me.blue.Size = New System.Drawing.Size(14, 13)
+        Me.blue.TabIndex = 6
+        Me.blue.TabStop = True
+        Me.blue.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Panel1.Location = New System.Drawing.Point(28, 20)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(16, 16)
+        Me.Panel1.TabIndex = 1
+        '
+        'red
+        '
+        Me.red.AutoSize = True
+        Me.red.Location = New System.Drawing.Point(12, 22)
+        Me.red.Name = "red"
+        Me.red.Size = New System.Drawing.Size(14, 13)
+        Me.red.TabIndex = 0
+        Me.red.TabStop = True
+        Me.red.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.input)
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 69)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(515, 162)
+        Me.GroupBox4.TabIndex = 4
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Text Area"
+        '
+        'input
+        '
+        Me.input.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.input.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.input.Location = New System.Drawing.Point(3, 16)
+        Me.input.Name = "input"
+        Me.input.Size = New System.Drawing.Size(509, 143)
+        Me.input.TabIndex = 1
+        Me.input.Text = ""
+        '
+        'GroupBox6
+        '
+        Me.GroupBox6.Controls.Add(Me.Label2)
+        Me.GroupBox6.Location = New System.Drawing.Point(277, 237)
+        Me.GroupBox6.Name = "GroupBox6"
+        Me.GroupBox6.Size = New System.Drawing.Size(250, 36)
+        Me.GroupBox6.TabIndex = 2
+        Me.GroupBox6.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(150, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Dinocan#9725 in VALORANT"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(539, 286)
+        Me.Controls.Add(Me.GroupBox6)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(555, 325)
+        Me.MinimumSize = New System.Drawing.Size(555, 325)
+        Me.Name = "Form1"
+        Me.Text = "VALORANT Coloured Text Generator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox6.ResumeLayout(False)
+        Me.GroupBox6.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents GroupBox6 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents input As RichTextBox
+    Friend WithEvents red As RadioButton
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents yellow As RadioButton
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents blue As RadioButton
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents green As RadioButton
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents pink As RadioButton
+    Friend WithEvents clsBrackets As RadioButton
+End Class
---
+++ VALORANT Coloured Text Generator/Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports System.Globalization
+
+Public Class Form1
+    Enum TextColours
+        Red
+        Blue
+        Yellow
+        Green
+        Pink
+        EndStatement
+    End Enum
+    Enum LanguageType
+        Tr
+        Eng
+    End Enum
+
+    Private Function GetHTMLColorCodeFromType(ByVal colorType As TextColours) As String
+        Select Case colorType
+            Case TextColours.Red
+                Return "enemy"
+            Case TextColours.Blue
+                Return "team"
+            Case TextColours.Yellow
+                Return "system"
+            Case TextColours.Green
+                Return "notification"
+            Case TextColours.Pink
+                Return "warning"
+            Case TextColours.EndStatement
+                Return "/"
+        End Select
+    End Function
+    Private Sub ChangeLang(ByVal lang As LanguageType)
+        Select Case lang
+            Case LanguageType.Tr
+                clsBrackets.Text = "Kapa"
+                GroupBox2.Text = "Dil"
+                GroupBox4.Text = "Yazı Alanı"
+                Me.Text = "VALORANT Renkli Yazı Oluşturucu"
+            Case LanguageType.Eng
+                clsBrackets.Text = "Close"
+                GroupBox2.Text = "Language"
+                GroupBox4.Text = "Text Area"
+                Me.Text = "VALORANT Coloured Text Generator"
+        End Select
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedIndex = 0 Then
+            ChangeLang(LanguageType.Tr)
+        ElseIf ComboBox1.SelectedIndex = 1 Then
+            ChangeLang(LanguageType.Eng)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If CultureInfo.CurrentCulture.EnglishName.StartsWith("Turkish") Then
+            ComboBox1.SelectedIndex = 0
+        Else
+            ComboBox1.SelectedIndex = 1
+        End If
+    End Sub
+
+    Private Sub AddColor(ByVal radioButton As RadioButton, ByVal ClrType As TextColours)
+        Dim rb As RadioButton = radioButton
+        Dim clr As TextColours = ClrType
+        Dim SL As Integer = input.SelectionLength
+            Dim SS As Integer = input.SelectionStart
+            If SL > 0 Then
+                input.Text = input.Text.Insert(SS + SL, "<" & GetHTMLColorCodeFromType(TextColours.EndStatement) & ">")
+                input.Text = input.Text.Insert(SS, "<" & GetHTMLColorCodeFromType(clr) & ">")
+            Else
+                input.Text = input.Text.Insert(SS, "<" & GetHTMLColorCodeFromType(ClrType) & ">")
+            End If
+        rb.Checked = False
+    End Sub
+
+
+    Private Sub red_CheckedChanged(sender As Object, e As EventArgs) Handles red.CheckedChanged
+        If red.Checked = True Then AddColor(red, TextColours.Red)
+        red.Checked = False
+    End Sub
+
+    Private Sub blue_CheckedChanged(sender As Object, e As EventArgs) Handles blue.CheckedChanged
+        If blue.Checked = True Then AddColor(blue, TextColours.Blue)
+        blue.Checked = False
+    End Sub
+
+    Private Sub yellow_CheckedChanged(sender As Object, e As EventArgs) Handles yellow.CheckedChanged
+        If yellow.Checked = True Then AddColor(yellow, TextColours.Yellow)
+        yellow.Checked = False
+    End Sub
+
+    Private Sub green_CheckedChanged(sender As Object, e As EventArgs) Handles green.CheckedChanged
+        If green.Checked = True Then AddColor(green, TextColours.Green)
+        green.Checked = False
+    End Sub
+
+    Private Sub pink_CheckedChanged(sender As Object, e As EventArgs) Handles pink.CheckedChanged
+        If pink.Checked = True Then AddColor(pink, TextColours.Pink)
+        pink.Checked = False
+    End Sub
+
+    Private Sub clsBrackets_CheckedChanged(sender As Object, e As EventArgs) Handles clsBrackets.CheckedChanged
+        If clsBrackets.Checked = True Then AddColor(clsBrackets, TextColours.EndStatement)
+        clsBrackets.Checked = False
+    End Sub
+End Class
